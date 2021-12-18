e08d96142958c4dca481eacc5461ebb79be997c3
beckzayi <beckzayi@126.com>
2021-12-18 23:57:52 +1100

    Container to wrap Header and Content

---
+++ src/components/Layout/Container.js
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  max-width: 100%;
+  margin: 0 auto;
+  padding: 0 1rem;
+
+  @media (min-width: 576px) {
+    max-width: 576px;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 768px;
+  }
+
+  @media (min-width: 992px) {
+    max-width: 992px;
+  }
+
+  @media (min-width: 1200px) {
+    max-width: 1200px;
+  }
+`;
+
+function Container(props) {
+  return <StyledContainer {...props} />;
+}
+
+export default Container;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,8 +2,13 @@ import React from "react";
 import { Layout } from "antd";
 import styled from "styled-components";
 import Header from "./Header";
+import Container from "./Container";
 
 const WrapperContent = styled.div`
+  padding: 0 60px;
+`;
+
+const WrapperChildren = styled.div`
   padding: 32px 0;
 `;
 
@@ -11,10 +16,16 @@ const PageLayout = ({ children }) => {
   return (
     <Layout className="layout">
       <Layout.Header>
-        <Header />
+        <Container>
+          <Header />
+        </Container>
       </Layout.Header>
-      <Layout.Content style={{ padding: "0 60px" }}>
-        <WrapperContent>{children}</WrapperContent>
+      <Layout.Content>
+        <WrapperContent>
+          <Container>
+            <WrapperChildren>{children}</WrapperChildren>
+          </Container>
+        </WrapperContent>
       </Layout.Content>
     </Layout>
   );
